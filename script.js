9c5fdeddec4ecb022d63f9dfc358f51a6529baba
Nicholas Kirk <Nick@Mac-mini.local>
2021-11-15 23:21:01 +0000

    show menu on click

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const open = document.querySelector('#open');
+const close = document.querySelector('#close');
+const container = document.querySelector('.container');
 
+open.addEventListener('click', function() {
+    container.classList.add('show-nav');
+})
