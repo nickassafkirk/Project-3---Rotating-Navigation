0931e50385b85b14423209557587a66152cc1d7d
Nicholas Kirk <Nick@Mac-mini.local>
2021-11-15 23:33:48 +0000

    Hide menu on click

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ const container = document.querySelector('.container');
 open.addEventListener('click', function() {
     container.classList.add('show-nav');
 })
+
+close.addEventListener('click', function() {
+    container.classList.remove('show-nav');
+})
+
